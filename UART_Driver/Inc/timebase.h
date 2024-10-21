6e0c762690f73fc413daf3fd4fb2b745c66b0be4
Hashaam Zafar <hashaam1217@gmail.com>
2024-10-21 10:51:54 -0700

    Clean up

--- UART_Driver/Inc/timebase.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TIMEBASE_H__
-#define __TIMEBASE_H__
-
-#include <stdint.h>
-
-void delay(uint32_t delay);
-void timebase_init(void);
-#endif
--- UART_Driver/Inc/uart.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UART_H__
-#define __UART_H__
-#include "stm32f446xx.h"
-#include <stdint.h>
-#include <stdio.h>
-
-void uart_tx_init(void);
-
-#endif
